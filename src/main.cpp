2ce14ec3b5bb80e49e6292a26851117e0fefa4f3
Phil <CppPhil@users.noreply.github.com>
2017-12-29 20:33:22 +0100

    UTF8 char demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../include/core_language/typename_in_template_template_parameter.hpp" // cpp17::core_language::typenameInTemplateTemplateParameterDemo
 #include "../include/core_language/auto_direct_list_initialization.hpp" // cpp17::core_language::autoDirectListInitializationDemo
 #include "../include/core_language/fold_expression.hpp" // cpp17::core_language::foldExpressionDemo
+#include "../include/core_language/utf8_literals.hpp" // cpp17::core_language::utf8LiteralsDemo
 #include <ios> // std::boolalpha
 #include <iostream> // std::cout
 
@@ -35,5 +36,6 @@ int main()
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::typenameInTemplateTemplateParameterDemo);
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::autoDirectListInitializationDemo);
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::foldExpressionDemo);
+    CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::utf8LiteralsDemo);
 }
 
