49b41ed76e25ce53eab0402ae91ea6682d0011cd
Phil <CppPhil@users.noreply.github.com>
2017-12-29 20:07:29 +0100

    Added fold expression demo

---
+++ include/core_language/fold_expression.hpp
@@ -0,0 +1,12 @@
+#ifndef INCG_CPP17_CORE_LANGUAGE_FOLD_EXPRESSION_HPP
+#define INCG_CPP17_CORE_LANGUAGE_FOLD_EXPRESSION_HPP
+
+namespace cpp17
+{
+namespace core_language
+{
+void foldExpressionDemo();
+} // namespace core_language
+} // namespace cpp17
+#endif // INCG_CPP17_CORE_LANGUAGE_FOLD_EXPRESSION_HPP
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../include/core_language/static_assert_with_no_message.hpp" // cpp17::core_language::staticAssertWithNoMessageDemo
 #include "../include/core_language/typename_in_template_template_parameter.hpp" // cpp17::core_language::typenameInTemplateTemplateParameterDemo
 #include "../include/core_language/auto_direct_list_initialization.hpp" // cpp17::core_language::autoDirectListInitializationDemo
+#include "../include/core_language/fold_expression.hpp" // cpp17::core_language::foldExpressionDemo
+#include <ios> // std::boolalpha
 #include <iostream> // std::cout
 
 #define CPP17_CALL_DEMO_FUNCTION(demoFunction) \
@@ -27,8 +29,11 @@ void callDemoFunctionImpl(
 
 int main()
 {
+    std::cout << std::boolalpha;
+
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::staticAssertWithNoMessageDemo);    
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::typenameInTemplateTemplateParameterDemo);
     CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::autoDirectListInitializationDemo);
+    CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::foldExpressionDemo);
 }
 
