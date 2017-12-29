56adc50a500a653768837d68bee69dc5d2aeed5d
Phil <CppPhil@users.noreply.github.com>
2017-12-29 16:42:01 +0100

    typename in template template parameter demo added

---
+++ include/core_language/typename_in_template_template_parameter.hpp
@@ -0,0 +1,12 @@
+#ifndef INCG_CPP17_CORE_LANGUAGE_TYPENAME_IN_TEMPLATE_TEMPLATE_PARAMETER_HPP
+#define INCG_CPP17_CORE_LANGUAGE_TYPENAME_IN_TEMPLATE_TEMPLATE_PARAMETER_HPP
+
+namespace cpp17
+{
+namespace core_language
+{
+void typenameInTemplateTemplateParameterDemo();
+} // namespace core_language
+} // namespace cpp17
+#endif // INCG_CPP17_CORE_LANGUAGE_TYPENAME_IN_TEMPLATE_TEMPLATE_PARAMETER_HPP
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include "../include/core_language/static_assert_with_no_message.hpp" // cpp17::core_language::staticAssertWithNoMessageDemo
+#include "../include/core_language/typename_in_template_template_parameter.hpp" // cpp17::core_language::typenameInTemplateTemplateParameterDemo
+#include <iostream> // std::cout
+
+#define CPP17_CALL_DEMO_FUNCTION(demoFunction) \
+    ::cpp17::detail::callDemoFunctionImpl(demoFunction, #demoFunction)
+
+namespace cpp17
+{
+namespace detail
+{
+namespace
+{
+template <typename NullaryCallable>
+void callDemoFunctionImpl(
+    NullaryCallable nullaryCallable, 
+    const char *functionName) noexcept
+{
+    std::cout << "BEGIN demo: "   << functionName << '\n';
+    nullaryCallable();
+    std::cout << "\nEND demo:   " << functionName << "\n\n"; 
+}
+} // anonymous namespace
+} // namespace detail
+} // namespace cpp17
 
 int main()
 {
-    cpp17::core_language::staticAssertWithNoMessageDemo();    
+    CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::staticAssertWithNoMessageDemo);    
+    CPP17_CALL_DEMO_FUNCTION(cpp17::core_language::typenameInTemplateTemplateParameterDemo);
 }
 
