f69a5cc1a9847ce2331fdd8a32b7feea26d30aed
Phil <CppPhil@users.noreply.github.com>
2018-01-06 17:21:32 +0100

    fix for structured bindings demo

--- src/core_language/structured_bindings.cpp
+++ src/core_language/structured_bindings.cpp
@@ -1,10 +1,11 @@
 #include "../../include/core_language/structured_bindings.hpp"
 #include <cstddef> // std::size_t
 #include <iostream> // std::cout
-#include <utility> // std::pair
+#include <utility> // std::pair, std::move
 #include <string> // std::string, std::to_string
 #include <array> // std::array
-#include <tuple> // std::tuple
+#include <tuple> // std::tuple, std::tuple_size, std::tuple_element
+#include <type_traits> // std::integral_constant
 
 // paper: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0217r3.html
 
@@ -17,32 +18,71 @@ namespace
 class MyClass
 {
 public:
+    using this_type = MyClass;
+
     explicit MyClass(int i)
         : m_i{ i },
           m_s{ std::to_string(m_i) + " text" }
     {
     }
 
+    template <std::size_t Idx>
+    auto &get() & noexcept
+    {
+        if constexpr (Idx == 0U) {
+            return m_i;
+        } else if constexpr (Idx == 1U) {
+            return m_s;
+        }
+    }
+
+    template <std::size_t Idx>
+    const auto &get() const & noexcept
+    {
+        return const_cast<this_type *>(this)->get<Idx>();
+    }
+
+    // reference collapsing will always result in rvalue reference here.
+    // This is the overload for rvalue MyClass objects.
+    template <std::size_t Idx>
+    auto &&get() && noexcept
+    {
+        if constexpr (Idx == 0U) {
+            return std::move(m_i);
+        } else if constexpr (Idx == 1U) {
+            return std::move(m_s);            
+        }
+    }
+
+private:
     int m_i;
     std::string m_s;
 };
+} // namespace cpp17
+} // namespace core_language
+} // anonymous namespace
 
-template <std::size_t Idx>
-auto &get(MyClass &myClass) noexcept
+namespace std
 {
-    if constexpr (Idx == 0U) {
-        return myClass.m_i;
-    } else if constexpr (Idx == 1U) {
-        return myClass.m_s;
-    } 
-}
-
-template <std::size_t Idx>
-const auto &get(const MyClass &myClass) noexcept
+template <>
+class tuple_size<::cpp17::core_language::MyClass>
+    : public integral_constant<size_t, 2U>
 {
-    return get<Idx>(const_cast<MyClass &>(myClass));
-}
+};
 
+template <size_t Idx>
+class tuple_element<Idx, ::cpp17::core_language::MyClass>
+    : public tuple_element<Idx, tuple<int, string>>
+{
+};
+} // namespace std
+
+namespace cpp17
+{
+namespace core_language
+{
+namespace
+{
 struct Structure
 {
     int i;
